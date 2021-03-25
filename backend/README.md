e66e58f15cef3a211c8e6fe532cc6fde438f8132
Shiv Shankar Kanaujiya <73483959+shivprime94@users.noreply.github.com>
2021-03-25 13:50:00 +0530

    First time I realised It is lot more difficult to built real world project.
    Thank you,
    Nikhil Agrawal

---
+++ backend/README.md
@@ -0,0 +1,18 @@
+#Sample Application Demonstration Purpose
+#Using Python Flask JWT Token and ReactJS.
+
+Clone the Repo
+- git clone git@github.com:MakeApps/react-flask.git
+- cd react-flask
+- pip install flask 
+- pip install Flask-JWT
+- pipenv shell
+
+Run the Flask Server 
+- FLASK_APP=app.py FLASK_ENV=development flask run
+
+#Front End 
+Install npm packages.
+- npm install
+- npm run watch
+
---
+++ backend/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+users = [
+    User(1, 'milind', 'milind'),
+    User(2, 'mike', 'mike'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+#app = Flask(__name__)
+app = Flask(__name__, template_folder='static/dist')
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+jwt = JWT(app, authenticate, identity)
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
+
+if __name__ == '__main__':
+    app.run()
